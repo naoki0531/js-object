41b4ad8128dce46afbf2417cb9176dd2c21484cd
丸山直樹 <andvari100@gmail.com>
2017-11-05 19:57:09 +0900

    add css

--- card.js
+++ card.js
@@ -2,13 +2,10 @@ var APP = APP || {};
 APP.Card = function (todoData) {
   this.todoData = todoData;
   this.card = document.importNode(document.querySelector('#card-template'), true).content;
-  this.init();
+  this.card.querySelector('.no').textContent = this.todoData.no;
+  this.card.querySelector('.title').textContent = this.todoData.title;
 };
 APP.Card.prototype = {
-  init: function () {
-    this.card.querySelector('.no').textContent = this.todoData.no;
-    this.card.querySelector('.title').textContent = this.todoData.title;
-  },
   getHtml: function () {
     return this.card;
   }
